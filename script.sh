dabe39b89c14fc8ffc8f90bd72f58e73fe7f456c
krisf85 <105288109+krisf85@users.noreply.github.com>
2022-05-11 18:48:07 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,3 +8,22 @@ read age
 
 echo "Hello $name, you are $age years old."
 
+echo "Do you want to be rich?"
+
+read rich
+
+echo "You said $rich to become rich. Do you want to know in how many years you will be rich?"
+
+read rich
+
+echo "You will be rich in ..."
+
+echo "Loading .."
+
+echo ".."
+
+echo "..."
+
+echo "....."
+
+echo "$RANDOM years!!"
